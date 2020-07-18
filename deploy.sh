83b18ce96545d22b4d2b424d94f2e7ecf5a9cd14
曾季暘(ShawnTseng) <shawntseng40@gmail.com>
2020-07-18 13:15:38 +0800

    deploy test

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
+npm run build
+cd dist
 git init
 git add -A
 git commit -m 'deploy'
-# 部署到 https://github.com/ShawnTseng/protfolio.git 分支為 gh-pages
-git push -f https://github.com/ShawnTseng/protfolio.git master:gh-pages
+# 部署到 https://github.com/ShawnTseng/vue-protfolio.git 分支為 gh-pages
+git push -f https://github.com/ShawnTseng/vue-protfolio.git master:gh-pages
