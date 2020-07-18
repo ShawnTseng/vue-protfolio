c5facc92953584da26539cfc3f7e14eee12814e3
曾季暘(ShawnTseng) <shawntseng40@gmail.com>
2020-07-18 13:31:23 +0800

    add running number animation

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <template>
   <div id="app">
     <TicTacToe />
+    <br />
+    <RunningNumberContainer />
   </div>
 </template>
 
 <script>
-import TicTacToe from './TicTacToe.vue'
+import TicTacToe from './components/TicTacToe.vue'
+import RunningNumberContainer from './components/RunningNumberContainer.vue'
 
 export default {
   name: 'App',
   components: {
-    TicTacToe
+    TicTacToe,
+    RunningNumberContainer
   }
 }
 </script>
---
+++ src/components/RunningNumberContainer.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2>{{count}}</h2>
+    <RunningNumber :number="count" />
+    <br />
+    <button @click="count+=100">+</button>
+    <button @click="count-=100">-</button>
+  </div>
+</template>
+
+<script>
+import RunningNumber from "./RunningNumber.vue";
+
+export default {
+  components: { RunningNumber },
+  data() {
+    return {
+      count: 0
+    };
+  }};
+</script>
+
+<style lang="scss" scoped>
+</style>
